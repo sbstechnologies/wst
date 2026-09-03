f9c8806a216e7ecb80d028d10224d7b1db6fac54
Babu Subramanian <softdreams4u@gmail.com>
2026-09-03 06:56:09 +0530

    Enter

--- app/components/TourScheduler.tsx
+++ app/components/TourScheduler.tsx
@@ -54,11 +54,14 @@ export default function TourScheduler({ open, onClose }: TourSchedulerProps) {
         </button>
 
         {/* Scheduler */}
+
         <iframe
-          src="https://charles.rentbamboo.com/embed/schedule?clientId=bamboo_7mkc8jx3&propertyId=0fe25d45-61b8-428e-b563-788155a1f8e3&color=1e3872"
-          title="Schedule a Tour"
+          src="https://charles.rentbamboo.com/embed/schedule?clientId=bamboo_1l359uhj&color=1E3872"
+          width="100%"
+          height="720"
           className="h-full w-full border-0"
           loading="lazy"
+          title="Schedule a tour"
         />
       </div>
     </div>
